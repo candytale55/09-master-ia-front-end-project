996e724426e1c5ff364295f4625b84ff3fc2bb3a
CandyTale55 <candytale55@gmail.com>
2026-02-02 23:31:27 +0100

    fix: update import path for defineConfig in vite.config.ts

--- simple-product-shop/vite.config.ts
+++ simple-product-shop/vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 // ^ Enables TypeScript intellisense for Vitest's 'test' config option
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
